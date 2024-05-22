be6d7066042547bdb00b8e7bfc6e3a55810028e4
柒子小姐 <itheima@itcast.cn>
2024-05-22 15:15:46 +0800

    header的基本布局和样式的编写

--- src/components/mainbox/TopHeader.vue
+++ src/components/mainbox/TopHeader.vue
@@ -1,13 +1,75 @@
 <template>
   <el-header>
-  <button @click="handleCollapsed">click</button>
-  Header
+    <div class="left">
+      <el-icon @click="handleCollapsed">
+        <Menu />
+      </el-icon>
+      <span style="margin-left: 10px">企业门户管理系统</span>
+    </div>
+    <div class="right">
+      <span style="margin-right: 10px">欢迎 admin 回来</span>
+      <el-dropdown>
+        <span class="el-dropdown-link">
+          <el-icon :size="30"><user /></el-icon>
+          <el-icon class="el-icon--right">
+            <arrow-down />
+          </el-icon>
+        </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item @click="handleCenter">个人中心</el-dropdown-item>
+            <el-dropdown-item @click="handeLogout">退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
   </el-header>
 </template>
 <script setup>
-import { useStore } from 'vuex';
-const store=useStore()
-const handleCollapsed =()=>{
-  store.commit("changeCollapsed")
+import { useStore } from 'vuex'
+import { Menu, User } from '@element-plus/icons-vue'
+import { useRouter } from 'vue-router'
+const store = useStore()
+const handleCollapsed = () => {
+  store.commit('changeCollapsed')
+}
+//编写路由跳转
+const router = useRouter()
+const handleCenter = () => {
+  router.push('/center')
+}
+const handeLogout = () => {
+  localStorage.removeItem('token')
+  router.push('/login')
 }
 </script>
+<style lang="scss" scoped>
+.el-header {
+  background-color: #2d3a4b;
+  color: wheat;
+  width: 100%;
+  height: 60px;
+  line-height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.left,
+.right {
+  display: flex;
+}
+.left {
+  i {
+    margin: auto;
+    cursor: pointer;
+  }
+}
+.right {
+  .el-dropdown {
+    margin: auto;
+  }
+}
+.el-icon--right {
+  color: black;
+}
+</style>
