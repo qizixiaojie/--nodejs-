d1127539495cb26bd356a0b23f5e9fd4c32a21ea
柒子小姐 <itheima@itcast.cn>
2024-07-03 22:46:40 +0800

    删除指定医院

--- src/api/hospital/index.js
+++ src/api/hospital/index.js
@@ -4,3 +4,11 @@ import request from "@/util";
 export const reqHospial_add = (data) => {
   return request.post('/admin_hospital/hospital_add', data);
 };
+//获取医院列表信息
+export const reqHospial_getList = () => {
+  return request.get('/admin_hospital/hospital_getList');
+};
+//删除医院列表细心呐
+export const reqHospial_Delete = (id) => {
+  return request.post('/admin_hospital/hospital_delete', { id });
+};
--- src/views/Hospital/HospitalList.vue
+++ src/views/Hospital/HospitalList.vue
@@ -1,5 +1,88 @@
 <template>
-  <div>
-    NewsList
+  <div class="main" v-if="hospital_data">
+    <el-card
+      style="width: 1200px"
+      class="card"
+      shadow="always"
+      v-for="item in hospital_data"
+      :key="item.id"
+    >
+      <img
+        :src="`data:image/jpeg;base64,${item.logoData}`"
+        alt=""
+        style="width: 50px"
+        v-if="item.logoData"
+      />
+      <span style="font-size: 24px; color: #1296db" v-else>暂无图片</span>
+      <span>医院名字：{{ item.hosname }}</span>
+      <span>所在地：{{ item.address }}</span>
+      <span>介绍：{{ item.intro }} </span>
+      <el-button class="delet" @click="detel(item.id)">删除</el-button>
+    </el-card>
+    <el-dialog v-model="dialogVisible" title="Tips" width="500">
+      <span>你确定要删除这个数据吗？</span>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button @click="dialogVisible = false">取消</el-button>
+          <el-button type="primary" @click="pushData"> 确定 </el-button>
+        </div>
+      </template>
+    </el-dialog>
   </div>
 </template>
+
+<script setup>
+import { reqHospial_getList, reqHospial_Delete } from "@/api/hospital";
+import { onMounted, ref } from "vue";
+import { ElMessage } from "element-plus";
+
+onMounted(() => {
+  getData();
+});
+
+const hospital_data = ref();
+const id = ref(0); //要删除的id
+const dialogVisible = ref(false); //是否展示弹窗
+//获取医院数据函数
+const getData = async () => {
+  const result = await reqHospial_getList();
+  if (result.code == 200) {
+    hospital_data.value = result.data;
+  }
+};
+
+//触发删除函数
+const detel = (itemId) => {
+  dialogVisible.value = true;
+  id.value = itemId;
+  console.log(id.value);
+};
+//触发真正的删除函数
+const pushData = () => {
+  dialogVisible.value = false;
+  ElMessage({
+    message: "删除成功",
+    type: "success",
+  });
+  reqHospial_Delete(id.value);
+  setTimeout(() => {
+    location.reload();
+  }, 3000);
+};
+</script>
+
+<style scoped>
+.main {
+  .card {
+    display: flex;
+    font-size: 20px;
+    margin-top: 20px;
+    span {
+      margin-left: 20px;
+    }
+    .delet {
+      margin-left: 100px;
+    }
+  }
+}
+</style>
